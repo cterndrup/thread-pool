7f599de1018d58954b36c7b37fc461a5259994ad
Colin Terndrup <cterndrup@cs.ucla.edu>
2017-06-07 21:02:57 -0700

    remove old files

--- thread_pool_task.h
+++
@@ -1,9 +0,0 @@
-/* Header file for thread pool task */
-
-#ifndef _THREAD_POOL_TASK_H_
-#define _THREAD_POOL_TASK_H_
-
-typedef void * thread_function_t(void *);
-typedef void * thread_callback_t(void *);
-
-#endif // _THREAD_POOL_TASK_H_
